895009c3e4a946c380b8af7beecef6220a0bcb6c
diana080299 <132782216+diana080299@users.noreply.github.com>
2023-09-17 12:38:28 +0100

    updat

---
+++ src/js/03-feedback.js
@@ -0,0 +1,37 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  input: document.querySelector('input'),
+};
+const formData = {
+  email: '',
+  message: '',
+};
+populateTextarea();
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+  localStorage.removeItem(STORAGE_KEY);
+  e.currentTarget.reset();
+  console.log(formData);
+});
+function onTextareaInput(e) {
+  formData[e.target.name] = e.target.value;
+  const stringifiedData = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, stringifiedData);
+}
+function populateTextarea() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedMessage === null) {
+    return;
+  }
+
+  refs.textarea.value = savedMessage.message || '';
+  refs.input.value = savedMessage.email || '';
+  formData.email = savedMessage.email || '';
+  formData.message = savedMessage.message || '';
+}
